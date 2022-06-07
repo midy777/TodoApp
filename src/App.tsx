76f15b85ca8300862eeb611af7ce78feac434e50
midy777 <marinaidyt777@gmail.com>
2022-06-07 18:25:59 +0700

    test

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { AddNewItem } from "./AddNewItem"
+import { Column } from "./Column"
+import { AppContainer } from "./styles"
+import { useAppState } from "./state/AppStateContext";
+
+export const App = () => {
+  const {lists} = useAppState()
+  return (
+    <AppContainer>
+      {lists.map(list => (
+        <Column text={list.text} key={list.id} id={list.id}/>
+      ))}
+      
+      <AddNewItem 
+        toggleButtonText="+ Add another list"
+        onAdd={(e) => console.log(e)} 
+      />
+    </AppContainer>
+  );
+}
+
